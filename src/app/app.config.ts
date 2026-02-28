e0fcf2260bd155f7f7bcd6b912549b9f01f67904
Sagate77 <kiliancorpet@gmail.com>
2026-02-28 13:33:30 +0100

    Front Accueil et Graphique en cours

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GraphComponent } from './graph/graph.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
+    provideRouter([
+      { path: '', component: HomeComponent },
+      { path: 'graphique', component: GraphComponent },
+    ])
   ]
 };
