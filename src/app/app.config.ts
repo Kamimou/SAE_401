7677e2e30be2f685c0d50e8c74a6d1ede2335e77
Oceane-Lufundu <lufundu.oceane@gmail.com>
2026-03-13 14:29:23 +0100

    modification de la structure plus ajout du header et footer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { GraphComponent } from './graph/graph.component';
+import { Home } from './component/home/home';
+import { Graph } from './component/graph/graph';
+import { Header } from './component/header/header';
+import { Footer } from './component/footer/footer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideRouter([
-      { path: '', component: HomeComponent },
-      { path: 'graphique', component: GraphComponent },
+      { path: '', component: Home },
+      { path: 'app-graph', component: Graph },
+      { path: 'app-header', component: Header },
+      { path: 'app-footer', component: Footer },
     ])
   ]
 };
